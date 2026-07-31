86d577cfc818676ab0b79581a19df1090170ddf1
poketomcraft-22 <poketomcraft22@gmail.com>
2026-07-31 10:50:31 +0200

    Modif

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,2 @@
 aws_region   = "us-east-1"
 project_name = "tp2-nginx-web"
-environment  = "dev"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,7 @@
 variable "aws_region" {
   type        = string
   description = "AWS region where resources will be deployed"
+  default     = "us-east-1"
 }
 
 variable "project_name" {
@@ -10,15 +11,5 @@ variable "project_name" {
   validation {
     condition     = can(regex("^[a-z0-9-]+$", var.project_name))
     error_message = "The project name must contain only lowercase letters, numbers, and hyphens."
-  }
-}
-
-variable "environment" {
-  type        = string
-  description = "Deployment environment (dev, test, prod)"
-
-  validation {
-    condition     = can(regex("^(dev|test|prod)$", var.environment))
-    error_message = "The environment must be 'dev', 'test', or 'prod'."
   }
 }
